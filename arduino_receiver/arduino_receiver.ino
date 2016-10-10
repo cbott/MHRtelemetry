2c13d4416d73bb7ec688c2bb393a27ca982546b2
cbott <cbott6@gmail.com>
2016-10-10 09:52:08 -0400

    some basic graphing stuff, still need to test

---
+++ arduino_receiver/arduino_receiver.ino
@@ -0,0 +1,86 @@
+#include <LiquidCrystal.h>
+#include <IRLib.h>
+
+#define PROTOCOL NEC
+#define BTN_UP 0x1FE58A7
+#define BTN_LEFT 0x1FE807F
+#define BTN_RIGHT 0x1FEC03F
+#define BTN_DOWN 0x1FEA05F
+
+#define SYMBOL_CHECKER byte(0)
+#define SYMBOL_BAD byte(1)
+#define SYMBOL_GOOD byte(2)
+#define SYMBOL_DOTS byte(3)
+
+const int SIGNAL_PIN = 6;
+
+IRrecv receiver(SIGNAL_PIN);
+IRdecode decoder;
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+
+byte bad[8] = {
+ 0b0,0b11011,0b1110,0b100,0b1110,0b11011,0b0,0b0
+};
+byte good[8] = {
+    0b00000,0b00000,0b00001,0b00010,0b10100,0b01000,0b00000,0b00000
+};
+
+void setup() {
+    Serial.begin(9600);
+    receiver.enableIRIn();
+
+    // set up the LCD's number of columns and rows:
+    lcd.begin(20, 4);
+  
+    lcd.createChar(0, checker);
+    lcd.createChar(1, bad);
+    lcd.createChar(2, good);
+    lcd.createChar(3, dots);
+}
+
+int val = 10;
+void loop() {
+    String result = "None";
+  if (receiver.GetResults(&decoder)) {
+    decoder.decode();       //Decode the data
+    if(decoder.decode_type == NEC){
+        switch(decoder.value){
+            case BTN_UP:
+              result = "Up";
+              val += 10;
+              break;
+            case BTN_DOWN:
+              result = "Down";
+              val -= 10;
+              break;
+            case BTN_LEFT:
+              result = "Left";
+              val -= 1;
+              break;
+            case BTN_RIGHT:
+              result = "Right";
+              val += 1;
+              break;
+            default:
+              result = "Error";
+              decoder.DumpResults();
+        }
+    } else {
+        decoder.DumpResults();  //Show the results on serial monitor
+    }
+    receiver.resume();      //Restart the receiver
+  }
+  
+  if(result != "None"){
+    lcd.clear();
+    if(result == "Error"){
+        lcd.write(SYMBOL_BAD);
+    } else {
+        lcd.write(SYMBOL_GOOD);
+    }
+    lcd.print(result);
+    lcd.display();
+  }
+  Serial.println(val);
+  delay(300);
+}
---
+++ liveplots.py
@@ -0,0 +1,50 @@
+#
+# This is a wrapper on some matplotlib functions to allow easy creation
+# of graphs that can have dynamic data in them
+#
+
+import argparse
+import numpy as np
+import random
+import sys
+
+from time import sleep
+from collections import deque
+
+import matplotlib.pyplot as plt 
+
+class ScrollingLinePlot:
+  """ Creates a line plot that shows a fixed number of
+      data points at a time, scrolling right to left """
+  def __init__(self, axis, title, ymin=0, ymax=100, width=100, ylabel="Data"):
+    self.axis = axis
+    self.axis.set_title(title)
+    self.axis.set_xlabel("Time (s)")
+    self.axis.set_ylabel(ylabel)
+    self.axis.set_xlim(-width,0)
+    self.axis.set_ylim(ymin, ymax)
+    self.data = deque([0]*width, maxlen = width)
+    self.line = self.axis.plot(self.data)[0]
+    self.line.set_data(range(-len(self.data)+1,1), self.data)
+
+  def append_data(self, val):
+    """ Add data and update the graph """
+    self.data.append(val) # deque automatically truncates to [width] values
+    self.line.set_ydata(self.data)
+
+class BarChart:
+    pass
+
+def line_plot_example():
+    """ Graph random data to show what the library can do """
+    fig, axes = plt.subplots(nrows = 1, ncols = 2)
+    graph_a = ScrollingLinePlot(axes[0], "Graph A", 0, 100, 10)
+    graph_b = ScrollingLinePlot(axes[1], "Graph B", 0, 10, 20)
+
+    plt.ion() #Make plot interactive
+    for i in range(100):
+        graph_a.append_data(random.randint(0,100))
+        graph_b.append_data(i%10)
+        plt.show()
+        plt.pause(0.1)
+#line_plot_example()
